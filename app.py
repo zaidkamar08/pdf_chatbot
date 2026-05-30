1c1ae48e845270654b370b219ab7e71b0ac52bb6
Md Jaaid Kamar <zaidkamar08@gmail.com>
2026-05-30 14:54:46 +0530

    Add assistant response to session state messages
    Append assistant's response to session messages.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.messages import HumanMessage, SystemMessage
 import tempfile
